f878e3f858da5b2a06fa8db6902e580a3cde9f49
MTBakalov18 <MTBakalov18@codingburgas.bg>
2022-11-07 14:03:32 +0200

    Commit

---
+++ src/main/java/com/example/classwork711/Classwork711Application.java
@@ -0,0 +1,13 @@
+package com.example.classwork711;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classwork711Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Classwork711Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/classwork711/Classwork711ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classwork711;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Classwork711ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
